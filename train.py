dc16dc5e438399da5a8d1bf5e7cf36d9d2858f70
Terbe Dániel <terbed@gmail.com>
2020-03-08 17:13:05 +0100

    close experiment when finished

--- train.py
+++ train.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
 
             model.seen += imgs.size(0)
 
-        if epoch % opt.evaluation_interval == 0:
+        if epoch % opt.evaluation_interval == 0 and epoch > 0:
             print("\n---- Evaluating Model ----")
             # Evaluate the model on the validation set
             precision, recall, AP, f1, ap_class = evaluate(
